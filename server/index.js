35e7e8d8aca3f7f823e1033b853695a1b9d5bdad
Caleb's PC <criggs2892@flpoly.org>
2017-06-08 11:42:01 -0400

    Adjusted index and added passport.js

--- server/index.js
+++ server/index.js
@@ -1,10 +1,14 @@
 var express = require('express');
+var passport = require('passport');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
 var app = express();
 var fs = require('fs');
 var MongoClient=require('mongodb').MongoClient;
 var config;
 try {
-	config = JSON.parse(fs.readFileSync('authorization.json', 'utf8'));
+	config = JSON.parse(fs.readFileSync('./config/authorization.json', 'utf8'));
 } catch (e) {
 	console.log('No config file found. Using defaults.');
 }
